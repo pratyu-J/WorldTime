d0c4f1e1f0baf97174191794323440d2d9080f1e
pratyush <pratyush021j@gmail.com>
2021-04-02 23:49:37 +0530

    flags added

--- lib/location.dart
+++ lib/location.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/card_template.dart';
 import 'package:flutter_app/services/world_time.dart';
 
+
 class Location extends StatefulWidget {
   @override
   _LocationState createState() => _LocationState();
@@ -11,15 +12,15 @@ class Location extends StatefulWidget {
 class _LocationState extends State<Location> {
 
   List<WorldTime> locations = [
-    WorldTime(url: 'Europe/London', location: 'London', flag: 'avatar.jpg'),
-    WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'avatar.jpg'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'england.jpg'),
+    WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'berlin.png'),
     WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'avatar.jpg'),
     WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'avatar.jpg'),
     WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'avatar.jpg'),
     WorldTime(url: 'Africa/Abidjan', location: 'Abidjan', flag: 'avatar.jpg'),
     WorldTime(url: 'Africa/Casablanca', location: 'Casablanca', flag: 'avatar.jpg'),
     WorldTime(url: 'Africa/Tripoli', location: 'Tripoli', flag: 'avatar.jpg'),
-    WorldTime(url: 'America/Argentina/Buenos_Aires', location: 'Buenos_Aires', flag: 'avatar.jpg'),
+    WorldTime(url: 'America/Argentina/Buenos_Aires', location: 'Buenos_Aires', flag: 'argentina.jpg'),
     WorldTime(url: 'America/Barbados', location: 'Barbados', flag: 'avatar.jpg'),
     WorldTime(url: 'America/Costa_Rica', location: 'Costa_Rica', flag: 'avatar.jpg'),
     WorldTime(url: 'America/Denver', location: 'Denver', flag: 'avatar.jpg'),
@@ -31,19 +32,20 @@ class _LocationState extends State<Location> {
     WorldTime(url: 'Asia/Baghdad', location: 'Baghdad', flag: 'avatar.jpg'),
     WorldTime(url: 'Asia/Beirut', location: 'Beirut', flag: 'avatar.jpg'),
     WorldTime(url: 'Asia/Bangkok', location: 'Bangkok', flag: 'avatar.jpg'),
-    WorldTime(url: 'Asia/Kolkata', location: 'Kolkata', flag: 'avatar.jpg'),
+    WorldTime(url: 'Asia/Kolkata', location: 'Kolkata', flag: 'india.jpg'),
     WorldTime(url: 'Asia/Kuala_Lumpur', location: 'Kuala_Lumpur', flag: 'avatar.jpg'),
     WorldTime(url: 'Asia/Qatar', location: 'Qatar', flag: 'avatar.jpg'),
-    WorldTime(url: 'Asia/Tokyo', location: 'Tokyo', flag: 'avatar.jpg'),
-    WorldTime(url: 'Australia/Adelaide', location: 'Adelaide', flag: 'avatar.jpg'),
-    WorldTime(url: 'Australia/Brisbane', location: 'Brisbane', flag: 'avatar.jpg'),
-    WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'avatar.jpg'),
+    WorldTime(url: 'Asia/Tokyo', location: 'Tokyo', flag: 'japan.png'),
+    WorldTime(url: 'Australia/Adelaide', location: 'Adelaide', flag: 'australia.jpg'),
+    WorldTime(url: 'Australia/Brisbane', location: 'Brisbane', flag: 'australia.jpg'),
+    WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'australia.jpg'),
   ];
 
   void TimeUpdate(index) async{
     WorldTime worldTime = locations[index];
 
     await worldTime.getTime();
+
     Navigator.pop(context, {
       'location': worldTime.location,
       'flag': worldTime.flag,
